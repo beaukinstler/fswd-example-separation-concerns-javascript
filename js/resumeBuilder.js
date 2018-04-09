2806206f695c4103fae51156a02fe5362d902626
Beau Kinstler <git@beaukinstler.info>
2018-04-08 21:57:51 -0400

    Add examples of separations of concerns

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,66 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+$(function() {
+    var model = {
+        init: function(){
+            this.projects = [{
+                "id":"1",
+                "name":"project1",
+                "shortDescription":"Simple project",
+                "longDescription":"Made stuff work while doing stuff"
+            },
+            {}];
+        },
+        getProjects(){
+            return this.projects;
+        }
+
+    };
+
+    var controller = {
+        init: function(){
+            model.init();
+            viewProjects.init();
+            
+
+        },
+        projects: function(){
+            return model.getProjects();
+        }
+
+    };
+
+    var viewMain = {
+
+    }
+
+    var viewPastJobs = {
+
+    }
+
+    var viewProjects = {
+        init: function(){
+            this.projectList = $("#projects");
+            this.initialHTML = this.projectList.html();
+            this.projectList.show();
+            viewProjects.render();
+        },
+
+        render: function(){
+            var html = this.initialHTML + "<div id='divProjects'>";
+            projects  = controller.projects();
+            for (elem in projects){
+                html += "<h5>Name: " + projects[elem].name + " || Short Desc: " + projects[elem].shortDescription;
+                html += "</h5>"
+            }
+            html += '</div>'
+            this.projectList.html( html );
+        }
+    }
+
+    // run the initializers
+    controller.init();
+});
